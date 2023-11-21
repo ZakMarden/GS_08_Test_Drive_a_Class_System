4266f9b79dabfb5f63d9158b8472fc3cd21e5376
Zak Marden <zakmarden@hotmail.com>
2023-11-21 12:40:19 +0000

    Todo DONE

--- lib/todo_list.py
+++ lib/todo_list.py
@@ -1,5 +1,6 @@
 class TodoList:
     def __init__(self):
+        self.todolist = []
         pass
 
     def add(self, todo):
@@ -9,21 +10,24 @@ class TodoList:
         #   Nothing
         # Side-effects:
         #   Adds the todo to the list of todos
+        self.todolist.append(todo)
         pass
 
     def incomplete(self):
         # Returns:
         #   A list of Todo instances representing the todos that are not complete
-        pass
+        return [x for x in self.todolist if x.complete == False]
 
     def complete(self):
         # Returns:
         #   A list of Todo instances representing the todos that are complete
-        pass
+        return [x for x in self.todolist if x.complete == True]
 
     def give_up(self):
         # Returns:
         #   Nothing
         # Side-effects:
         #   Marks all todos as complete
+        for task in self.todolist:
+            task.mark_complete()
         pass
--- tests/test_todo.py
+++ tests/test_todo.py
@@ -1,5 +1,54 @@
 from lib.todo import *
 from lib.todo_list import *
 
-def test_todo():
-    pass
+def test_todo_blank_task():
+    todo = Todo("")
+    assert todo.task == ""
+    assert todo.complete == False
+
+def test_todo_complete_blank_task():
+    todo = Todo("")
+    todo.mark_complete()
+    assert todo.complete == True
+
+def test_todolist_blank_list():
+    todolist = TodoList()
+    assert todolist.todolist == []
+
+def test_todolist_add_string():
+    todolist = TodoList()
+    todolist.add("test string")
+    assert todolist.todolist == ["test string"]
+
+def test_integration_todolist_add():
+    todolist = TodoList()
+    todo = Todo("test task")
+    todolist.add(todo)
+    assert todolist.todolist == [todo]
+
+def test_integration_todolist_return_incomplete():
+    todolist = TodoList()
+    todo = Todo("test task")
+    todolist.add(todo)
+    assert todolist.incomplete() == [todo]
+    assert todolist.complete() == []
+
+def test_integration_todolist_return_complete():
+    todolist = TodoList()
+    todo = Todo("test task")
+    todolist.add(todo)
+    todo.mark_complete()
+    assert todolist.complete() == [todo]
+    assert todolist.incomplete() == []
+
+def test_integration_todolist_give_up():
+    todolist = TodoList()
+    todo = Todo("test task")
+    todo2 = Todo("test task2")
+    todo3 = Todo("test task3")
+    todolist.add(todo)
+    todolist.add(todo2)
+    todolist.add(todo3)
+    assert todolist.complete() == []
+    todolist.give_up()
+    assert todolist.complete() == [todo, todo2, todo3]
